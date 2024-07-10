2b347b419c7dc73d0d2c738c7ef4fa0b1a82e896
Archana-bio <kaviyaveeramuthu2@gmail.com>
2024-07-10 15:59:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var on_button=document.getElementById("On");
+var off_button=document.getElementById("Off");
+var image=document.getElementById("bulb");
+
+function bulb_on(){
+    image.src="./image/bulb_on.jpeg";
+}
+function bulb_off(){
+    image.src="./image/bulb_off.jpeg";
+}
